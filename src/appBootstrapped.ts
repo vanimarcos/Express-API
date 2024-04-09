006a19ead4265f487310c18414f1fcc49604c0e7
vanimarcos <stackvany@gmail.com>
2024-04-09 12:52:36 +0100

    hide powerd-by in http header

--- src/appBootstrapped.ts
+++ src/appBootstrapped.ts
@@ -1,18 +1,19 @@
 import 'dotenv/config';
 import express from 'express';
 import helmet from 'helmet';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const timeout = require('connect-timeout');
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const hpp = require('hpp');
 import cors from 'cors';
 import { corsOptions } from './cors';
 import routes from './routes';
 import app from './app';
 import { RequestTimeout } from './utils/middewares';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const timeout = require('connect-timeout');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const hpp = require('hpp');
 
 /** Bootstrap application */
 app.use(helmet());
+app.use(helmet.hidePoweredBy());
 app.use(RequestTimeout);
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '1mb' }));
